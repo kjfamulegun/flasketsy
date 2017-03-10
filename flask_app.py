a10cbdfae03f51937b9a765d32715f630c31c407
kjfamulegun <kansolafamuz@live.com>
2017-03-10 22:35:26 +0000

    Midterm Complete

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,30 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from wtforms import Form, StringField, DecimalField, TextAreaField
+from wtforms.validators import DataRequired
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/kjfamulegun/sites/etsydemo/tmp/database.db'
+db = SQLAlchemy(app)
+app.secret_key = 'development key'
+
+class Listing(db.Model):
+    __tablename__ = "course"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float(2))
+class ListingForm(Form):
+    name = StringField('name', validators=[DataRequired()])
+    price = DecimalField('price')
+    description = TextAreaField('description')
+
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    results = Listing.query.filter(1==1).all()
+    return render_template('index.html', listings=results)
 
 @app.route('/about')
 def about():
@@ -13,3 +32,55 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+
+@app.route('/listing/new', methods=['GET', 'POST'])
+def newlisting():
+    form = ListingForm(request.form)
+    if request.method == 'POST':
+        if form.validate() == False:
+          flash('All fields are required.')
+          return render_template('newlisting.html', form=form)
+        else:
+          lst = Listing(name=form.name.data, description= form.description.data, price=form.price.data )
+          db.session.add(lst)
+          db.session.commit()
+          results = Listing.query.filter(1==1).all()
+          return render_template('index.html', listings=results)
+    else:
+        return render_template('newlisting.html', form=form)
+
+@app.route('/listing/show/<listing_id>')
+def listing_show(listing_id):
+    try:
+        lst = Listing.query.get(listing_id)
+    except:
+        abort(404)
+    return render_template('listing_show.html', list_id=lst.id, listing=lst)
+@app.route('/listing/edit/<listing_id>', methods=['GET','POST'])
+def listing_edit(listing_id):
+    form = ListingForm(request.form)
+    if request.method == 'POST':
+        if form.validate() == False:
+            flash('All fields are required.')
+            return render_template('listing_edit.html', form=form)
+        else:
+            lst = Listing.query.get(listing_id)
+            lst.name=form.name.data
+            lst.description= form.description.data
+            lst.price=form.price.data
+            db.session.commit()
+            flash('Your listing was edited.')
+            return render_template('newlisting.html', form=form)
+    else:
+        try:
+            lst = Listing.query.get(listing_id)
+            lstForm = ListingForm(obj=lst)
+        except IOError as e:
+            abort(404)
+        return render_template('listing_edit.html', form=lstForm)
+@app.route('/listing/delete/<listing_id>')
+def listing_delete(listing_id):
+    Listing.query.filter_by(id=listing_id).delete()
+    db.session.commit()
+    return render_template('listing_delete_confirm.html', listid=listing_id)
